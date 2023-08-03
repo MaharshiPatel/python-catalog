bf3be410b10b978823e01f372accd5743e4edede
Maharshi Patel <maharship@jfrog.com>
2023-08-03 07:43:33 -0700

    udpated code

--- pythonExample/pythonProj.py
+++ pythonExample/pythonProj.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+import numpy as np
 
 # Function definition is here
 def printme( str ):
     # This prints a passed string into this function
     print (str)
+    rng = np.random.default_rng()
+    samples = rng.normal(size=2500)
+    print (samples)
     return;
 
 # Now you can call printme function
