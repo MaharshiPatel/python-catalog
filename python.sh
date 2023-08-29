236a872f0cfc92d10a8c04320a830e68d61598b0
Maharshi <Maharshi11patel@gmail.com>
2023-08-29 11:33:15 -0700

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
 python setup.py sdist
-
 jf rt u dist/ alpha-pypi-virtual/pythonWithDockerEnvironment/ --build-name=${Build.DefinitionName} --build-number=${Build.BuildNumber} --module=jfrog-python-example --props=stage=dev
