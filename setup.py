baab8c1cc7cdf06d2acd43e9967426d3cbe97b52
Maharshi <Maharshi11patel@gmail.com>
2024-08-29 11:31:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='jfrog-python-example',
-    version='10.0',
+    version='11.0',
     description='Project example for building Python project with JFrog products',
     author='JFrog',
     url='https://github.com/jfrog/project-examples',
